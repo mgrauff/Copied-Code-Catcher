b199d0154357c6b260aefc89d7ec5c6f106c4910
Enoch Andreades <57719394+Helethel@users.noreply.github.com>
2021-03-09 15:31:32 -0500

    Added beginnings of text UI

--- src/main.java
+++ src/main.java
@@ -1,9 +1,59 @@
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+//Alex Chen
+//Enoch Andreades
+//Luke Greenway
+//Matthew Grauff
+//Paul Staats
+//Copied Code Catcher ~~~ COMP 350 A
+
 
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		String filePath; //Where all the files are coming from
+		ArrayList<File> files = new ArrayList<File>(); //ArrayList for our files
+
+		boolean done = false; //When user has no more files to give
+
+		
+		Scanner scany = new Scanner(System.in); //Scanner for scaning the console
+
+		System.out.println("--------Copied Code Catcher--------\n");
+
+		System.out.print("Please enter file path to read from: "); //Asking for file path
+
+		filePath = scany.next(); //Recieving file path
+
+		while (!done) { //While user has more files
+
+			//Asking user for next file name
+			System.out.print("\nPlease enter file name (if finished please enter \"done\"): ");
+			
+			String input = scany.next(); //Getting file name
+
+			if (input.equalsIgnoreCase("done")) { done = true; } //Checking if user is done
+
+			else { //Put file in arraylist and process it
+
+				files.add(new File(filePath + "/" + input));
+				FileProcessor processor = new FileProcessor(files.get(files.size() - 1));
+
+				processor.read();
+
+			}
+
+		}
+
+		scany.close();
+
+
+
+		
 		File myFile = new File("C:/Users/StaatsPD17/git/Copied-Code-Catcher/Copied-Code-Catcher/src/example.txt");
 		FileProcessor myProcessor = new FileProcessor(myFile);
 		myProcessor.read();
