a1ca485eb9345d98c32852f915071f132f6943cb
lgree <lgree@DESKTOP-NE2RHLP.mshome.net>
2021-03-14 14:00:18 -0400

    revamped comparison algorithm. Now uses legit statistical method.

--- src/Compare.java
+++ src/Compare.java
@@ -13,31 +13,48 @@ public class Compare {
 	
 	
 	public double compareFiles() {
-		double score = 0;
-		double ratio;
+		//Calculate 1 - RMSE to find similarity
+		double scaleSum = 0.0;
 		int num1,num2, sum;
 		int numOps = 0;
+		int maxUse =0;
+		for(String op:operators.operatorList) {
+			num1 = f1.mapGet(op);
+			num2 = f2.mapGet(op);
+			if(num1> maxUse) {
+				maxUse = num1;
+			}
+			if(num2>maxUse) {
+				maxUse = num2;
+			}
+		}
+		
+		//This shouldn't be possible
+		if(maxUse == 0) {
+			System.out.println("ERROR: No operators detected");
+			return -1;
+		}
+		
 		for(String op: operators.operatorList) {
 			num1 = f1.mapGet(op);
 			num2 = f2.mapGet(op);
 			sum = num1+num2;
+			double scale1,scale2;
+			scaleSum = 0.0;
 			//If they're not used, we shouldn't include them in the avg
 			if(sum > 0) {
-				//if both numbers aren't used, who cares
-				if(num2 != 0 ) {
-					ratio = num1/num2;
-				}
-				else {
-					ratio = 1;
-				}
-				score+= sum*ratio;
+				scale1 = num1/(1.0*maxUse);
+				scale2 = num2/(1.0*maxUse);
+				//Get difference squared
+				scaleSum += Math.pow(Math.abs(scale1-scale2), 2);
 				numOps ++;
 			}
 
 			
 		}
 		
-		return score/numOps;
+		
+		return 1.0 - Math.sqrt(scaleSum/(numOps*1.0));
 	}
 	
 	/*
