2ff99576bc52bdc7c5e80fe36ccfcd3c1b6fa6d1
Enoch Andreades <57719394+Helethel@users.noreply.github.com>
2021-03-18 14:45:50 -0400

    Main fixing

--- src/Operators.java
+++ src/Operators.java
@@ -9,7 +9,7 @@ public class Operators {
 	//That could mean Operators is a bad name for the class... But are you gonna complain?
 	
 	public Operators(String operatorsFileName) throws FileNotFoundException {
-		File file = new File("src/"+operatorsFileName);
+		File file = new File(operatorsFileName);
 		Scanner scnr = new Scanner(file);
 		operatorList = new ArrayList<String>();
 		while(scnr.hasNext()) {
