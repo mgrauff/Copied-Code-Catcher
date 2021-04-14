4db5acac76b030e781cfcdf8714e14a76bb30646
Paul_Staats <paul.d.staats@gmail.com>
2021-04-14 12:48:20 -0400

    Attempt at setting up a JUnit testHarness

---
+++ src/files/TestHarness.java
@@ -0,0 +1,14 @@
+package files;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestHarness {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
