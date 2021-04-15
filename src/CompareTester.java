ee287fe80a6d2bc3d1161b473cd6d81908e7f27c
Enoch Andreades <57719394+Helethel@users.noreply.github.com>
2021-04-15 12:33:30 -0400

    Adding Junit and tester classes

---
+++ src/CompareTester.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CompareTester {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/FileProcessorTester.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FileProcessorTester {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
