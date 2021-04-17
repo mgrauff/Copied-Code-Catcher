e1404c265055050ff563350210529284755fe3f3
Enoch Andreades <57719394+Helethel@users.noreply.github.com>
2021-04-16 21:03:31 -0400

    Added self building isZipped Test for Unzipper

--- src/UnzipperTester.java
+++ src/UnzipperTester.java
@@ -1,17 +1,50 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
 import org.junit.jupiter.api.Test;
 
 class UnzipperTester {
 
+	//Testing isZipped methods
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void isZippedTest() throws IOException {
+		
+		/* Making zipped files to use to test */
+		File zFile = new File("src/files/MyZip.txt");
+		zFile.createNewFile();
+		
+		String zipFileName = zFile.getName().concat(".zip");
+		 
+        FileOutputStream fos = new FileOutputStream(zipFileName);
+        ZipOutputStream zos = new ZipOutputStream(fos);
+
+        zos.putNextEntry(new ZipEntry(zFile.getName()));
+
+        byte[] bytes = Files.readAllBytes(Paths.get("src/files/MyZip.txt"));
+        zos.write(bytes, 0, bytes.length);
+        zos.closeEntry();
+        zos.close();
+        
+        //Test
+        assertTrue(Unzipper.isZipped("MyZip.txt.zip"));
+        
+        //Deleting the files
+        File zipFile = new File("MyZip.txt.zip");
+		zFile.delete();
+		zipFile.delete();
 	}
 	
+	//Testing unzip to method TODO
 	@Test
-	void testingTest() {
-		
+	void unzipTest() {
+		fail("TODO");
 	}
-
 }
