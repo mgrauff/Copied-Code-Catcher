891f1e927af565f4b4a2248eb88570a5142f3309
Enoch Andreades <57719394+Helethel@users.noreply.github.com>
2021-04-19 19:06:00 -0400

    Finished UnzipperTester and added documentation

--- src/UnzipperTester.java
+++ src/UnzipperTester.java
@@ -1,11 +1,14 @@
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Scanner;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -21,11 +24,12 @@ class UnzipperTester {
 		File zFile = new File("src/files/MyZip.txt");
 		zFile.createNewFile();
 		
+		//Add zip tag
 		String zipFileName = zFile.getName().concat(".zip");
-		 
+		
+		//Zip up file
         FileOutputStream fos = new FileOutputStream(zipFileName);
         ZipOutputStream zos = new ZipOutputStream(fos);
-
         zos.putNextEntry(new ZipEntry(zFile.getName()));
 
         byte[] bytes = Files.readAllBytes(Paths.get("src/files/MyZip.txt"));
@@ -44,7 +48,53 @@ class UnzipperTester {
 	
 	//Testing unzip to method TODO
 	@Test
-	void unzipTest() {
-		fail("TODO");
+	void unzipTest() throws IOException {
+		
+		/* Making zipped files to use to test */
+		File zFile = new File("src/files/MyZip.txt");
+		zFile.createNewFile();
+		
+		//Write Hello World to the file
+		FileWriter fw = new FileWriter(zFile);
+		fw.write("Hello World!");
+		fw.close();
+		
+		//Add zip tag
+		String zipFileName = zFile.getName().concat(".zip");
+		 
+		//Zip up file
+        FileOutputStream fos = new FileOutputStream(zipFileName);
+        ZipOutputStream zos = new ZipOutputStream(fos);
+        zos.putNextEntry(new ZipEntry(zFile.getName()));
+
+        byte[] bytes = Files.readAllBytes(Paths.get("src/files/MyZip.txt"));
+        zos.write(bytes, 0, bytes.length);
+        zos.closeEntry();
+        zos.close();
+        zFile.delete();
+        
+        //Call unzipper
+        Unzipper unZippy = new Unzipper("MyZip.txt.zip");
+        
+        //Unzip my zipped file
+        unZippy.unzipTo("src/files/");
+        
+        //Turn the file contents into a string
+        File unzedFile = new File("src/files/Myzip.txt");
+        Scanner scany = new Scanner(unzedFile);
+        String str = scany.nextLine();
+        scany.close();
+        
+        //Deleting the files
+        unzedFile.delete();
+        File zipFile = new File("MyZip.txt.zip");
+		zipFile.delete();
+        
+		//Test that text has not changed
+        assertTrue(str.equals("Hello World!"));
+        
+
+       
+		
 	}
 }
