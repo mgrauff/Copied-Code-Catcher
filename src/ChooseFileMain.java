1ff65cf58ab6c4ad88f092aff2daf083183c335c
Paul_Staats <paul.d.staats@gmail.com>
2021-04-29 14:28:15 -0400

    Added some documentation to ChooseFileMain

--- src/ChooseFileMain.java
+++ src/ChooseFileMain.java
@@ -3,7 +3,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
-import javafx.scene.control.TextArea;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
@@ -11,15 +10,11 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
-//import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-
 import javax.swing.JOptionPane;
-
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -31,27 +26,13 @@ public class ChooseFileMain extends Application implements EventHandler<ActionEv
 	Stage window;
 	Button selectFilesButton;
 	Button unzipToDestFileButton;
-	Button addDirectoryButton;
-	Button removeFileButton;
-	ListView<String> fileList;
-	ObservableList<String> data = FXCollections.observableArrayList();
+	Button addDirectoryButton; //adds all the files in a given directory to the fileList
+	Button removeFileButton; //button for removing files from the fileList
+	ListView<String> fileList; //text area displaying all files in the ChooseFile object's selectedFile list
+	ObservableList<String> data = FXCollections.observableArrayList(); //stores the items in fileList
 	ChooseFile fc = new ChooseFile();
 	
 	public void start(Stage primaryStage) {
-		//
-		/*
-		try {
-			Parent root = FXMLLoader.load(getClass().getResource("/application/ChooseFile.fxml"));
-			Scene scene = new Scene(root);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		}catch(Exception e) {
-			e.printStackTrace();
-		}*/
-		
-		
-		//
 		
 		window = primaryStage;
 		window.setTitle("Copy-Code-Catcher by Team Robin");
@@ -60,58 +41,61 @@ public class ChooseFileMain extends Application implements EventHandler<ActionEv
 		grid.setVgap(8);
 		grid.setHgap(10);
 		
-		selectFilesButton = new Button("Add Files");
+		//selectFilesButton
+		//TODO fix formatting if multiple files are selected
+		selectFilesButton = new Button("Add File(s)");
 		selectFilesButton.setOnAction(this);
 		GridPane.setConstraints(selectFilesButton, 1, 2);
 		
+		//unziptoDestFileButton
 		unzipToDestFileButton = new Button("Unzip to dest File");
 		unzipToDestFileButton.setOnAction(this);
 		GridPane.setConstraints(unzipToDestFileButton, 2, 3);
 		
+		//addDirectoryButton
+		//TODO fix no files selected exception handling
 		addDirectoryButton = new Button("Add Directory");
 		addDirectoryButton.setOnAction(this);
 		GridPane.setConstraints(addDirectoryButton, 3, 4);
 		
+		//removeFileButton
 		removeFileButton = new Button("Remove File(s)");
 		removeFileButton.setDisable(true);
 		removeFileButton.setOnAction(this);
 		GridPane.setConstraints(removeFileButton, 4, 5);
 		
-		fileList = new ListView();
-		fileList.setItems(data);
-		fileList.setEditable(false);
+		fileList = new ListView<String>();
+		fileList.setItems(data); //populate the fileList
+		fileList.setEditable(false);//we don't want the user messing with the list
 		fileList.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+		//Only have the remove file button active when something is selected
 		fileList.setOnMouseClicked(new EventHandler<MouseEvent>() {
 
 			@Override
 			public void handle(MouseEvent arg0) {
-				// TODO Auto-generated method stub
 				removeFileButton.setDisable(false);
-			}
+			}//handle
 			
-		});
+		});//end fileList eventHandler
 		
 		
+		//Label the fileList area
 		Label fileListLabel = new Label("File List");
 		Font font = Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 12);
 	    fileListLabel.setFont(font);
 	    
-		VBox ncf = new VBox(fileListLabel, fileList, selectFilesButton, unzipToDestFileButton, addDirectoryButton, removeFileButton);
-		ncf.setSpacing(20);
+		//Set up the window visually
+	    VBox ncf = new VBox(fileListLabel, fileList, selectFilesButton, unzipToDestFileButton, addDirectoryButton, removeFileButton);
+	    ncf.setSpacing(20);
 		ncf.setPadding(new Insets(20, 50, 50, 60));
-		
-		
 		grid.getChildren().addAll(ncf);
-		
 		Scene scene = new Scene(grid, 500, 500);
 		window.setScene(scene);
+		window.setMaximized(true);
 		window.show();
-	}
+		
+	}//end start
 
-	public static void main(String[] args){
-		launch(args);
-
-	}
 
 	@Override
 	public void handle(ActionEvent event) {
@@ -150,9 +134,10 @@ public class ChooseFileMain extends Application implements EventHandler<ActionEv
 				}
 			} catch (Exception e) {}
 		}
+	}//end handle
+	
+	public static void main(String[] args){
+		launch(args);
 	}
 
-	
-
-
-}
+ }//end ChooseFileMain
